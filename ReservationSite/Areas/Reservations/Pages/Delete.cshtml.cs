c37cda92f65c8a0e974a5f9754b6e53b37f15fec
Herrick Spencer <herricks@microsoft.com>
2019-03-31 14:30:37 -0700

    Add CRUDpages for reservations and move layouts and views to more global location

---
+++ ReservationSite/Areas/Reservations/Pages/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ReservationSite.Data;
+
+namespace ReservationSite.Areas.Reservations.Pages
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ReservationSite.Data.ReservationsDbContext _context;
+
+        public DeleteModel(ReservationSite.Data.ReservationsDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Reservation Reservation { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Reservation = await _context.Reservations.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Reservation == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Reservation = await _context.Reservations.FindAsync(id);
+
+            if (Reservation != null)
+            {
+                _context.Reservations.Remove(Reservation);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ReservationSite/Areas/Reservations/Pages/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ReservationSite.Data;
+
+namespace ReservationSite.Areas.Reservations.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ReservationSite.Data.ReservationsDbContext _context;
+
+        public DetailsModel(ReservationSite.Data.ReservationsDbContext context)
+        {
+            _context = context;
+        }
+
+        public Reservation Reservation { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Reservation = await _context.Reservations.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Reservation == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
